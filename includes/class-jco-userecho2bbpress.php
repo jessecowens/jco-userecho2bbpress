6238faa5ae8cf70a8ad377b4c6b76dbf848a60ea
Jesse C. Owens <jesse@jessecowens.com>
2020-10-08 17:35:48 -0600

    Remove public section

--- includes/class-jco-userecho2bbpress.php
+++ includes/class-jco-userecho2bbpress.php
@@ -77,7 +77,6 @@ class Jco_Userecho2bbpress {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -116,12 +115,6 @@ class Jco_Userecho2bbpress {
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-jco-userecho2bbpress-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-jco-userecho2bbpress-public.php';
-
 		$this->loader = new Jco_Userecho2bbpress_Loader();
 
 	}
@@ -157,22 +150,6 @@ class Jco_Userecho2bbpress {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_plugin_menu' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Jco_Userecho2bbpress_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 
 	}
 
