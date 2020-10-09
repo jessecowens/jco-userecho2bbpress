b84d36d0ab310780b25027954889ce4c76fbd25b
Jesse C. Owens <jesse@jessecowens.com>
2020-10-09 16:20:12 -0600

    Add phpDocs

--- admin/class-jco-userecho2bbpress-forum.php
+++ admin/class-jco-userecho2bbpress-forum.php
@@ -19,16 +19,42 @@
  */
 class Jco_Userecho2bbpress_Forum{
 
+  /**
+   * The base forum array
+   *
+   * @since    1.0.0
+   * @access   private
+   * @var      array    $forum    An array of all forums in the import.
+  */
   private $forum;
 
+  /**
+   * Initialize the class and decode the import file
+   *
+   * @since    1.0.0
+   * @param    string    $json_file      Path to the forums.json file
+   */
   public function __construct( $json_file ) {
     $this->forum = json_decode( file_get_contents( $json_file ), true );
   }
 
+  /**
+  * Get the full forum array utility
+  *
+  * @since    1.0.0
+  * @return   array  An array of forums from the import
+  */
   public function get_forum() {
     return $this->forum;
   }
 
+
+  /**
+  * Fetch an array of all the forums marked as PUBLIC
+  *
+  * @since    1.0.0
+  * @return   array   Array of all public forums as id=>name
+  */
   public function get_public_forums() {
     $public_forums = array();
     foreach ( $this->forum as $forum ) {
@@ -40,6 +66,12 @@ class Jco_Userecho2bbpress_Forum{
     return $public_forums;
   }
 
+  /**
+  * Find out how many topics are in a given forum.
+  *
+  * @since    1.0.0
+  * @return   int   Number of Topics in forum $id
+  */
   public function get_forum_topic_count( $id ) {
     $key = array_search( $id, array_column($this->forum, 'id') );
     return $this->forum[$key]['topic_count'];
